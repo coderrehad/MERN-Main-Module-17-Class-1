2826f95731ff4b490faeaa5ec34e1a0368c7c7f6
rehaddev <rehadhasanshawn2@gmail.com>
2023-12-11 11:17:22 +0600

    upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = require('./src/routes/api');
+const app = new express();
+
+
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose =require('mongoose');
+const path = require("path");
+
+
+let URL = "mongodb://localhost:27017/MERNEcommerce"
+
+mongoose.connect(URL).then(()=>{
+    console.log("Database Connect")
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+app.use(cookieParser());
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb"}));
+
+
+const Limiter = rateLimit({windowMs:15*60*1000, max:3000})
+app.use(Limiter);
+
+
+app.use("/api/v1", router);
+
+
+// Add React Front End Routing
+app.get('*',function (req,res) {
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
+
+
+module.exports = app;
+
