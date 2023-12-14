d3ca242cfd903e3dff143b5e023ac25eb14f5f5b
rehaddev <rehadhasanshawn2@gmail.com>
2023-12-14 16:41:14 +0600

    service manage

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,61 @@
+const {BrandListService} = require('../services/ProductServices');
+
+exports.ProductBrandList= async (req,res)=>{
+    let result = await BrandListService();
+    return res.status(200).json(result)
+}
+
+
+
+exports.ProductCategoryList= async (req,res)=>{
+    let result = await CategoryListService();
+    return res.status(200).json(result)
+}
+
+
+exports.ProductSliderList= async (req,res)=>{
+    let result = await SliderListService();
+    return res.status(200).json(result)
+}
+
+
+
+
+exports.ProductListByBrand= async (req,res)=>{
+    let result = await ListByBrandService();
+    return res.status(200).json(result)
+}
+
+
+exports.ProductListByCategory= async (req,res)=>{
+    let result = await ListByCategoryService();
+    return res.status(200).json(result)
+}
+
+
+exports.ProductListBySmilier= async (req,res)=>{
+
+}
+
+
+exports.ProductListByKeyword= async (req,res)=>{
+
+}
+
+
+
+exports.ProductListByRemark= async (req,res)=>{
+    let result = await ListByRemarkService();
+    return res.status(200).json(result)
+}
+
+
+exports.ProductDetails= async (req,res)=>{
+
+}
+
+
+
+exports.ProductReviewList=async(req,res)=>{
+
+}
